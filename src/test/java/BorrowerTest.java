6f7f56123d8dca49f0ae54815f7d82b03ca64a85
Marcin <marcinszydelko@yahoo.com>
2020-01-07 22:13:40 +0000

    add test canAddBookToCollection

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -6,14 +6,23 @@ import static org.junit.Assert.assertEquals;
 public class BorrowerTest {
 
     private Borrower borrower;
+    private Book book;
 
     @Before
     public void before() {
         borrower = new Borrower("John");
+        book = new Book("The Hobbit", "J. R. R. Tolkien", "fantasy");
     }
+
 
     @Test
     public void canCountBorrowedBooks() {
         assertEquals(0, borrower.countBorrowedBooks());
+    }
+
+    @Test
+    public void canAddBookToCollection() {
+        borrower.addBookToCollection(book);
+        assertEquals(1, borrower.countBorrowedBooks());
     }
 }
