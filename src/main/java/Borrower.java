3b8afc432415f60bbc4e0f994b7e42881adf4762
Marcin <marcinszydelko@yahoo.com>
2020-01-07 21:40:26 +0000

    setup Borrower class and test

---
+++ src/main/java/Borrower.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Borrower {
+    private String name;
+    private ArrayList<Book> booksBorrowed;
+
+    public Borrower(String name) {
+        this.name = name;
+        this.booksBorrowed = new ArrayList<Book>();
+    }
+}
---
+++ src/test/java/BorrowerTest.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class BorrowerTest {
+
+    private Borrower borrower;
+
+    @Before
+    public void before() {
+        borrower = new Borrower("John");
+    }
+
+    
+}
